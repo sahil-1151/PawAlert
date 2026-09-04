bc5be9a9327ab22d494ac5701d7154cd73abcdb0
sahil <sahilprajapati10001@gmail.com>
2026-09-04 11:03:38 +0530

    Add full CRUD route pattern in Crow(GET,POST,PATCH,DELETE)with dummy data

--- Backend/src/main.cpp
+++ Backend/src/main.cpp
@@ -11,5 +11,42 @@ int main(){
         response["status"] = "ok";
         return response;
     });
+    CROW_ROUTE(app, "/cases/<int>")([](int case_id){    //this is the get request for an case id -
+        crow::json::wvalue response;
+        response["case_id"] = case_id; 
+        response["status"] = "pending";
+        response["message"] = "This is the dummy data for now";
+        return response;
+    });
+
+    CROW_ROUTE(app, "/reports").methods(crow::HTTPMethod::POST)([](                //this is explicitly say it's post otherwise it will treat as get.
+        const crow::request& req){
+            auto body = crow::json::load(req.body);
+            if(!body){
+                return crow::response(400,"Invalid Json");
+            }
+            std::string animal_type = body["animal_type"].s();
+            std::string condition = body["condition"].s();
+            crow::json::wvalue response;
+            response["report_id"] = 101;
+            response["animal_type"] = animal_type;
+            response["condition"] = condition;
+            response["status"] = "pending";
+            return crow::response(201, response);
+                });
+            CROW_ROUTE(app,"/cases/<int>/verify").methods(crow::HTTPMethod::PATCH)([](int case_id){
+                crow::json::wvalue response;
+                response["case_id"] = case_id;
+                response["status"] = "verified";
+                response["message"] = "case has been verified by moderator";
+                return crow::response(200, response);
+            });
+            CROW_ROUTE(app, "/reports/<int>").methods(crow::HTTPMethod::DELETE)([](
+                int report_id){
+                    crow::json::wvalue response;
+                    response["report_id"]= report_id;
+                    response["message"]="report deleted successfully";
+                    return crow::response(200, response);
+                });
     app.port(8080).multithreaded().run();        //serve this server on port 8080 and we can handle multiple request which is imp for os.
 }
