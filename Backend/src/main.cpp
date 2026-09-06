24c9b082296a15234d3ed9f5fb98902ee761eddc
sahil <sahilprajapati10001@gmail.com>
2026-09-06 09:31:14 +0530

    Complete full CRUD for resource table (Create, Read, Update, Delete)

--- Backend/src/main.cpp
+++ Backend/src/main.cpp
@@ -60,7 +60,8 @@ int main(){
                 response["message"] = "case has been verified by moderator";
                 return crow::response(200, response);
             });
-    CROW_ROUTE(app,"/areas/<int>").methods(crow::HTTPMethod::PATCH)([](const crow::request& req, int area_id){
+
+    CROW_ROUTE(app,"/areas/<int>").methods(crow::HTTPMethod::PATCH)([](const crow::request& req, int area_id){ //patch request. 
         auto body = crow::json::load(req.body);
             if(!body) return crow::response(400, "INVALID JSON");
             std::string new_name = body["area_name"].s();
@@ -86,7 +87,8 @@ int main(){
                 return crow::response(400, error);
             }
     });
-    CROW_ROUTE(app,"/areas").methods(crow::HTTPMethod::POST)([](const crow::request& req){
+
+    CROW_ROUTE(app,"/areas").methods(crow::HTTPMethod::POST)([](const crow::request& req){ //post request.
         auto body = crow::json::load(req.body);
         if(!body) return crow::response(400, "INVALID JSON");
         std::string area_name = body["area_name"].s();
@@ -109,7 +111,8 @@ int main(){
             return crow::response(500, error);
             }
         });
-    CROW_ROUTE(app,"/areas")([](){
+
+    CROW_ROUTE(app,"/areas")([](){  // get request.
         try{
             pqxx::connection conn("dbname=pawalert user=" + std::string(getenv("USER")));
             pqxx::work txn(conn);
@@ -132,5 +135,106 @@ int main(){
             return crow::response(500, error);
         }
     });
+
+    CROW_ROUTE(app,"/resources").methods(crow::HTTPMethod::POST)([](const crow::request& req){  //post for resource.
+        auto body = crow::json::load(req.body);
+            if(!body) return crow::response(400, "INVALID JSON");
+            std::string resource_type = body["resource_type"].s();
+            std::string name = body["name"].s(); 
+            try{
+                pqxx::connection conn("dbname=pawalert user=" + std::string(getenv("USER")));
+                pqxx::work txn(conn);
+                pqxx::result r = txn.exec_params(
+                    "INSERT INTO resource (resource_type,name) VALUES ($1 , $2) RETURNING resource_id",
+                    resource_type,name
+                );
+                txn.commit();
+                crow::json::wvalue response;
+                response["resource_id"] = r[0][0].as<int>();
+                response["resource_type"] = resource_type;
+                response["name"] = name;
+                return crow::response(201,response);
+                }
+            catch(const std::exception& e){
+                crow::json::wvalue error;
+                error["error"] = e.what();
+                return crow::response(500 , error);
+            }
+    });
+
+    CROW_ROUTE(app, "/resources")([](){  //get request for resource.
+        try{
+           pqxx::connection conn("dbname=pawalert user=" + std::string(getenv("USER")));
+            pqxx::work txn(conn);
+
+            pqxx::result r = txn.exec("SELECT resource_id,resource_type,name,available FROM resource");
+            txn.commit(); 
+            
+            crow::json::wvalue response;
+            int i=0;
+            for(auto row : r){
+                response[i]["resource_id"] = row["resource_id"].as<int>();
+                response[i]["name"] = row["name"].c_str();
+                response[i]["resource_type"] = row["resource_type"].c_str();
+                response[i]["available"] = row["available"].c_str();
+                i++;
+            }
+            return crow::response(200, response);
+        }
+        catch(const std::exception& e){
+            crow::json::wvalue error;
+            error["error"] = e.what();
+            return crow::response(500, error);
+        }
+    });
+
+    CROW_ROUTE(app,"/resources/<int>").methods(crow::HTTPMethod::PATCH)([](
+        const crow::request& req,int resource_id){
+            auto body = crow::json::load(req.body);
+            if(!body) return crow::response(400, "INVALID JSON");
+            bool available = body["available"].b();
+            try{
+                pqxx::connection conn("dbname=pawalert user=" + std::string(getenv("USER")));
+                pqxx::work txn(conn);
+        
+                txn.exec_params(
+                "UPDATE resource SET available = $1 WHERE resource_id = $2",
+                available, resource_id
+        );
+        txn.commit();
+
+        crow::json::wvalue response;
+        response["resource_id"] = resource_id;
+        response["available"] = available;
+        return crow::response(200,response); 
+            }
+        catch(const std::exception& e){
+            crow::json::wvalue error;
+            error["error"] = e.what();
+            return crow::response(500,error);
+        } 
+        });
+    
+    CROW_ROUTE(app,"/resources/<int>").methods(crow::HTTPMethod::DELETE)([](int resource_id){
+        try{
+            pqxx::connection conn("dbname=pawalert user=" + std::string(getenv("USER")));
+            pqxx::work txn(conn);
+
+            txn.exec_params(
+                "DELETE FROM resource WHERE resource_id = $1",resource_id);
+            txn.commit();
+            
+            crow::json::wvalue response;
+            response["resource_id"] = resource_id;
+            response["message"] = "Resource deleted Suceesfully";
+            return crow::response(200, response);
+        }
+        catch(const std::exception& e){
+            crow::json::wvalue error;
+            error["error"]=e.what();
+            return crow::response(500, error);
+        }
+    });
+
     app.port(8080).multithreaded().run();        //serve this server on port 8080 and we can handle multiple request which is imp for os.
 }
